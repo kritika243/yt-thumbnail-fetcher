f6cd50d5c2721235778b505be3b9ebf7322e1c39
Kritika Sharma <kritikasharma@Kritikas-MacBook-Air.local>
2021-08-14 18:55:38 +0530

    making the header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='bg-red-800 rounded-b-2xl p-5 shadow-xl'>
+      <h1>Get YouTube Thumbnail</h1>
+    </header>
+  )
+}
+
+export default Header
