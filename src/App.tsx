717903a931407ef94041b7d79ae199a32295fea8
Kritika Sharma <kritikasharma@Kritikas-MacBook-Air.local>
2021-08-15 00:36:22 +0530

    final functionality added for getting thumbnail

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './components/Header'
 import InputUrl from './components/InputUrl'
+import Thumbnail from './components/Thumbnail'
 
 function App() {
+  const [videourl, setVideourl] = useState('')
+  const [thumbnailurl, setThumbnailurl] = useState('')
+
+  const submitHandler: React.FormEventHandler = (e) => {
+    e.preventDefault()
+    const idRegex =
+      /(?:youtube(?:-nocookie)?\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/
+    if (!videourl.match(idRegex)) return
+    const videoId = videourl.match(idRegex)![1]
+    setThumbnailurl(`https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`)
+  }
+
   return (
     <div>
       <Header />
       <main className='container mx-auto mt-8'>
-        <InputUrl />
+        <InputUrl
+          videourl={videourl}
+          setVideourl={setVideourl}
+          submitHandler={submitHandler}
+        />
+        <Thumbnail thumbnailUrl={thumbnailurl} />
       </main>
     </div>
   )
--- src/components/InputUrl.tsx
+++ src/components/InputUrl.tsx
@@ -1,20 +1,35 @@
 import React from 'react'
 import { FiLink, FiX } from 'react-icons/fi'
 
-const InputUrl = () => {
+interface Props {
+  videourl: string
+  setVideourl: React.Dispatch<React.SetStateAction<string>>
+  submitHandler: React.FormEventHandler
+}
+
+const InputUrl = ({ videourl, setVideourl, submitHandler }: Props) => {
+  const resetInput = () => {
+    setVideourl('')
+  }
+
   return (
-    <form className='relative flex'>
+    <form className='relative flex' onSubmit={submitHandler}>
       <FiLink className='absolute top-3 left-2 w-6 h-6' />
       <button
         type='button'
         className='absolute top-2 right-40 w-8 h-8 rounded hover:bg-gray-300'
       >
-        <FiX className='ml-2 ' />
+        <FiX className='ml-2 ' onClick={resetInput} />
       </button>
       <input
         className='border-2 border-gray-600 outline-none focus:border-red-600 focus:rounded-2xl rounded-lg p-2 pl-10 text-xl w-full transition-all mr-2 '
         type='url'
         placeholder='Enter video link here'
+        value={videourl}
+        // onChange={(e) => setVideourl(e.target.value)}
+        onChange={(e) => {
+          setVideourl(e.target.value)
+        }}
       />
       <button className='bg-red-600 text-white text-xl w-40 rounded-2xl hover:rounded-lg transition-all active:bg-red-500'>
         Get Now
