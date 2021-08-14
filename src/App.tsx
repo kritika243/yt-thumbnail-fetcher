174727af20719bad27221256807f58bb22c05fd4
Kritika Sharma <kritikasharma@Kritikas-MacBook-Air.local>
2021-08-14 19:21:32 +0530

    completed the header for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import Header from './components/Header'
 
 function App() {
-  return <div></div>
+  return (
+    <div>
+      <Header />
+      <main className='container mx-auto mt-8'></main>
+    </div>
+  )
 }
 
 export default App
